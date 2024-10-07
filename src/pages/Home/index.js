9dec6253ce0be30f8a15b2a9fff7c6ebd243a4b2
NiiTR0oX <benjamin.leblanc3426@hotmail.com>
2024-10-07 14:06:38 +0200

    last

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -117,15 +117,18 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+        {last ? (
             <EventCard
-              imageSrc={last?.cover}
-              title={last?.title}
-              date={new Date(last?.date)}
+              imageSrc={last.cover}
+              title={last.title}
+              date={new Date(last.date)}
               small
               label="boom"
               // label={last?.label}
             />
-        
+          ) : (
+            <p>Aucun événement récent</p>
+          )}   
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
